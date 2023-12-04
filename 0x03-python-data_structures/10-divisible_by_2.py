ee55649b48787995c8551ac8c1e2a43b7fe2411e
saharrrrr <skmao23@gmail.com>
2023-12-04 03:06:26 -0800

    listss

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            new_list.append(true)
+            new_list.append(True)
         else:
-            new_list.append(false)
+            new_list.append(False)
     return new_list
